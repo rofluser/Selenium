1291d20acccfafd518a579f3db8f98980e84b039
Your Name <haris.siddique@venturedive.com>
2022-06-05 00:40:34 +0500

    First commit

---
+++ src/main/java/test/LoginPage.java
@@ -0,0 +1,53 @@
+package test;
+
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void login() {
+        String login_url = "https://opensource-demo.orangehrmlive.com/";
+        driver.get(login_url);
+
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.xpath("//div[@id='divLoginButton']//input")).click();
+        driver.findElement(By.id("menu_dashboard_index")).click();
+        String textPresent = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/h1")).getText();
+
+        String textToBePresent = "Dashboard";
+        assertEquals(textPresent, textToBePresent);
+    }
+
+
+
+
+
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
